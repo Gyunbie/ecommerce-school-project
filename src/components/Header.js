e4b0165cb1da5e98af72d5934ac7a2f684757d83
gyunbie <gyunbie@gmail.com>
2022-01-08 17:50:11 +0300

    Add register/login buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,17 @@
-import { ShoppingBagIcon } from "@heroicons/react/outline";
+import {
+  ShoppingBagIcon,
+  UserAddIcon,
+  LoginIcon,
+} from "@heroicons/react/outline";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 
 function Header() {
   const [cart] = useState(JSON.parse(localStorage.getItem("cart")) || {});
+
+  const no_implementation = () => {
+    window.alert("This has not been implemented yet.");
+  };
 
   return (
     <div className="flex items-center h-[60px] shadow-md w-full">
@@ -32,17 +40,22 @@ function Header() {
         </Link>
       </div>
 
-      <Link
-        to="/cart"
-        className="mr-3 cursor-pointer flex-grow flex justify-end"
-      >
-        <div className="relative">
+      <div className="mr-3 cursor-pointer flex-grow flex items-center justify-end">
+        <div className="flex items-center border-2 border-blue-400 p-1 rounded-lg mx-3" onClick={no_implementation}>
+          <UserAddIcon className="h-4 w-4" />
+          <p className="font-semibold">Sign Up</p>
+        </div>
+        <div className="flex items-center border-2 border-blue-400 bg-blue-400 p-1 rounded-lg mx-3" onClick={no_implementation}>
+          <LoginIcon className="h-4 w-4" />
+          <p className="font-semibold">Sign In</p>
+        </div>
+        <Link className="relative" to="/cart">
           <ShoppingBagIcon className="h-10 w-10 p-1 relative" />
           <p className="absolute bottom-0 left-0 bg-red-500 h-5 w-5 text-center text-white rounded-full text-sm">
             {Object.keys(cart).length}
           </p>
-        </div>
-      </Link>
+        </Link>
+      </div>
     </div>
   );
 }
